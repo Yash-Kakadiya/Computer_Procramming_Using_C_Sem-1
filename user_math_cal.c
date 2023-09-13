579b99c9a76d696ea8afdfb5c1bf73f37551bdc6
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2023-09-13 19:29:17 +0530

    Add files via upload

--- user_math_cal.c
+++ user_math_cal.c
@@ -1,33 +1,34 @@
 #include<stdio.h>
 
 void main(){
-	int a,b,c;
-    printf("Enter 1 For Addtion \n");
-    printf("Enter 2 For Substraction \n");
-    printf("Enter 3 For Multiplication \n");
-    printf("Enter 4 For Division \n");
-    printf("Enter 5 For Modulo \n");
+	int a,b;
+    char c;
+    // printf("Enter 1 For Addtion \n");
+    // printf("Enter 2 For Substraction \n");
+    // printf("Enter 3 For Multiplication \n");
+    // printf("Enter 4 For Division \n");
+    // printf("Enter 5 For Modulo \n");
 	printf("Enter value : ");
 	scanf("%d",&a);
-    printf("Enter number for number Mathematical calculation : ");
-	    scanf(" %d",&c);
-	while(c!=0){
+    printf("Enter Arithmatic Sign : ");
+	    scanf(" %c",&c);
+	while(c!='0'){
          printf("Enter next value : ");
 	        scanf("%d",&b);
         switch(c){
-		case 1:
+		case '+':
         a=(a+b);
 			printf("Answer = %d\n",(a));
 			break;
-		case 2:
+		case '-':
 			a=(a-b);
 			printf("Answer = %d\n",(a));
 			break;
-		case 3:
+		case '*':
 			a=(a*b);
 			printf("Answer = %d\n",(a));
 			break;
-		case 4:
+		case '/':
 			if(b==0){
 			printf("Not Defined\n");
             printf("Promgram Restart\n");
@@ -39,18 +40,18 @@ void main(){
 			printf("Answer = %d\n",(a));
 		}
 			break;
-		case 5:	
+		case '%':	
 			a=(a%b);
 			printf("Answer = %d\n",(a));
 			break;
 		default:
-             c=0;
+             c='0';
 			printf("Invalid Number Mathematical calculation");
 			break;
 	}
     if(c!=0){
     printf("Enter number for Mathematical calculation : ");
-	    scanf(" %d",&c);
+	    scanf(" %c",&c);
     }
     }
 	
