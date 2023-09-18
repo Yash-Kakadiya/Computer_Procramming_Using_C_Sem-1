5a42a6ef25fe50de85ceb1068135791089d2cd08
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2023-09-18 19:43:52 +0530

    Update and rename 1_5_series.c to 1_50_series.c

--- 1_50_series.c
+++ 1_50_series.c
@@ -3,7 +3,7 @@
 void main(){
 	int i=-2;
 	
-	printf("Print first 50 numbers in series 1, 4, 7, 10… :\n" );
+	printf("Print first 50 numbers in series 1, 4, 7, 10â€¦ :\n" );
 	
 	while(i<=47){
 		printf("%d\n",i+=3);
