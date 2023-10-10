d4c1f987d35979ea67e859841d4281c44041514a
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2023-10-10 09:19:00 +0530

    Update reverse_word.c

--- reverse_word.c
+++ reverse_word.c
@@ -4,7 +4,7 @@ void main(){
     char w[100];
     int l=0,i;
     printf("Enter a word : ");
-    scanf("%s",w);
+    scanf("%s",&w);
 
     while(w[l]!='\0'){ //   '\0' because null char ='\0'
         l++;
