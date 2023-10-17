3e4e5bff06dcc3524039a64a0cd41db29004a2ed
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2023-10-17 09:00:24 +0530

    Add files via upload

---
+++ palcement.c.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void main(){
+	int n,same=1,i,x=0;
+	
+	printf("How Many Numbers you will Enter : ");
+	scanf("%d",&n);
+	
+	int a[n];
+	
+	for(i=0;i<n;i++){	
+		printf("Enter Number for a[%d] :",i);
+		scanf("%d",&a[i]);
+	}
+	printf("--------------------------------\n");
+	for(i=0;i<n-1;i++){
+		
+		if(a[i]>a[i+1]){
+			x=1;
+			break;
+		}
+	}
+	
+	for(i=0;i<n-1;i++){
+	
+		if(x!=1){
+			if(a[i]==a[i+1]){
+				same++;
+			}
+			
+			else if(same>=3){
+				same=1;
+			}
+			else{
+				break;
+			}
+		}
+		
+	}
+		if(same>=3){
+			printf("Yes");
+		}
+		else{
+			printf("No");
+		}
+	
+}	
+
